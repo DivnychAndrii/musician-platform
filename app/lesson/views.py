585888b9b76003fc9602580f13844cf346ef3a73
Andrew <divnycha05@gmail.com>
2019-05-21 19:09:17 +0100

    Seventh day lesson changes

--- app/lesson/views.py
+++ app/lesson/views.py
@@ -1,6 +1,7 @@
 from rest_framework import mixins, viewsets, status
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from app.pagination import ResultSetPagination
@@ -25,13 +26,14 @@ class LikeViewSet(mixins.ListModelMixin,
 
     serializer_class = LikesSerializer
     permission_classes = (IsGetOrAuthenticated,)
+    pagination_class = ResultSetPagination
+
+    def get_lesson_or_404(self):
+        return get_object_or_404(Lessons, id=self.kwargs['lesson_id'])
 
     def get_queryset(self):
-        queryset = Lessons.objects.get_queryset()
-        print(queryset)
-        if self.kwargs['lesson_id'] in queryset:
-            return Like.objects.filter(lesson_id=self.kwargs['lesson_id'])
-        return status.HTTP_404_NOT_FOUND
+        lesson = self.get_lesson_or_404()
+        return Like.objects.filter(lesson=lesson)
 
     @action(methods="POST", detail=False)
     def toggle(self, *_args, **_kwargs):
@@ -42,3 +44,4 @@ class LikeViewSet(mixins.ListModelMixin,
             like.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(LikesSerializer(like).data, status.HTTP_201_CREATED)
+
